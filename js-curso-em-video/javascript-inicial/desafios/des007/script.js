9edab27897bb39fbcfecf9262c3a2ece6c97b8d1
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-24 17:24:31 -0300

    js

--- js-curso-em-video/javascript-inicial/desafios/des007/script.js
+++ js-curso-em-video/javascript-inicial/desafios/des007/script.js
@@ -2,10 +2,10 @@
 let dolar = parseFloat(prompt("Qual a cotação do dolar? "))
 
 function convmoeda() {
-    let real = parseFloat(prompt("Quantos R$ você tem na carteira?"))
-    let cot = real / dolar
+    let real =  parseFloat(prompt("Quantos R$ você tem na carteira?"))
+    let cot = real.toFixed(2) / dolar.toFixed(2) 
 
     let res = document.querySelector("section#resultado")
 
-    res.innerHTML = `<P>Com ${real} R$ você consegue comprar ${cot}US$ </P>`
+    res.innerHTML = `<P>Com ${real.toFixed(2)} R$ você consegue comprar ${cot.toFixed(2)} US$ </P>`
 }

01f1ef80bb57f6f8c1df360f956ce82ec40c344c
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-25 17:15:52 -0300

    js

---
+++ js-curso-em-video/javascript-inicial/desafios/des0011/script.js
@@ -0,0 +1,10 @@
+
+let ano = Number(prompt("Digite um ano para ssaber se ele bissexto"))
+let res = document.querySelector("section#resultado")
+
+if ((ano / 4) % == 0) {
+    res.innerHTML = `<P> O ano ${ano} é bissexto </P>`
+} esif {
+    res.innerHTML = `<P> O ano ${ano} não é bissexto </P>`
+
+}

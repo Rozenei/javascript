d274946873e928722eb0e5255224aa578ae1e011
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-26 17:11:13 -0300

    js

--- js-curso-em-video/javascript-inicial/desafios/des0012/script.js
+++ js-curso-em-video/javascript-inicial/desafios/des0012/script.js
@@ -3,15 +3,35 @@ function mudapreco() {
 let precoant = parseFloat(prompt("Informe o preço anterior"))
 let precoatual = parseFloat(prompt("Informe o preço antual"))
 let preco = precoatual - precoant
-let variacao = precoatual % precoant
+let variacao = precoatual - precoant
 let porcerto = (variacao / precoatual) * 100
 
-let res = document.querySelector("section#resultado")/*
+let res = document.querySelector("section#resultado")
+
+
+if (precoant < precoatual) {
+    document.write( "<P> O produto custava R$ " + precoant.toFixed(2) + " e agora custa R$ " + precoatual.toFixed(2) + ".</P>")
+    document.write ( "<P>Hoje o produto está mais caro.</P>")
+    document.write("<P> O preço subiu R$ " + variacao.toFixed(2) + " .</P>")
+    document.write( "<P>Um aumento de " + variacao + "% </P>")
+} 
+if (precoant > precoatual) {
+    document.write( "<P> O produto custava R$ " + precoant.toFixed(2) + "e agora custa R$" + precoatual.toFixed(2) + ".</P>")
+    document.write ( "<P>Hoje o produto está mais barato.</P>")
+    document.write("<P> O preço baixou R$ " + variacao.toFixed(2) + "  .</P>")
+    document.write(" <P>Um variação de -" + variacao + "%</P>")
+
+    }
+}
+
+
+
+/*
 
 res.innerHTML = `<P> O produto custava R$ ${precoant.toFixed(2)} e agora custa R$ ${precoatual}.</P>`
 res.innerHTML += `<P>Hoje o produto está mais caro.</P>`
 res.innerHTML += `<P> O preço subiu R$ ${variacao.toFixed(2)}.</P>`
-res.innerHTML += `<P>Um aumento de ${porcerto.toFixed(2)}%</P>`*/
+res.innerHTML += `<P>Um aumento de ${porcerto.toFixed(2)}%</P>`
 
 if (precoant < precoant) {
     res.innerHTML = `<P> O produto custava R$ ${precoant} e agora custa R$ ${precoatual}.</P>`
@@ -24,6 +44,5 @@ else (precoant > precoatual) {
     res.innerHTML += `<P>Hoje o produto está mais barato.</P>`
     res.innerHTML += `<P> O preço baixou R$ .</P>`
     res.innerHTML += `<P>Uma variação de - %</P>`
-}
+}*/
 
-}

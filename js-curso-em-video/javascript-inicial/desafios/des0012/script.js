46586dc852f83589aeaa633559634c45383bad63
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-27 09:33:29 -0300

    js

--- js-curso-em-video/javascript-inicial/desafios/des0012/script.js
+++ js-curso-em-video/javascript-inicial/desafios/des0012/script.js
@@ -2,27 +2,34 @@
 function mudapreco() {
 let precoant = parseFloat(prompt("Informe o preço anterior"))
 let precoatual = parseFloat(prompt("Informe o preço antual"))
-let preco = precoatual - precoant
 let variacao = precoatual - precoant
-let porcerto = (variacao / precoatual) * 100
+let porcento = (variacao / precoant) * 100
 
 let res = document.querySelector("section#resultado")
 
-porcetagem
+  //porcetagem
+  //valor é X por cento de Y  =  (valor / Y ) * 100
 
-valor é X por cento de Y  =  (100 / X) * Y
-
+if (variacao < 0){
+    variacao = variacao * -1
+}
 if (precoant < precoatual) {
     document.write( "<P> O produto custava R$ " + precoant.toFixed(2) + " e agora custa R$ " + precoatual.toFixed(2) + ".</P>")
+    
     document.write ( "<P>Hoje o produto está mais caro.</P>")
+
     document.write("<P> O preço subiu R$ " + variacao.toFixed(2) + " .</P>")
-    document.write( "<P>Um aumento de " + variacao + "% </P>")
+
+    document.write( "<P>Um aumento de " + porcento.toFixed(2) + "% </P>")
 } 
 if (precoant > precoatual) {
     document.write( "<P> O produto custava R$ " + precoant.toFixed(2) + "e agora custa R$" + precoatual.toFixed(2) + ".</P>")
+
     document.write ( "<P>Hoje o produto está mais barato.</P>")
+
     document.write("<P> O preço baixou R$ " + variacao.toFixed(2) + "  .</P>")
-    document.write(" <P>Um variação de -" + variacao + "%</P>")
+
+    document.write(" <P>Um variação de " + porcento.toFixed(2) + "%</P>")
 
     }
 }

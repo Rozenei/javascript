b119be42630ad69d6cfd236a98f08192690038b7
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-27 17:23:31 -0300

    js

--- js-curso-em-video/javascript-inicial/exercicios/ex013/script.js
+++ js-curso-em-video/javascript-inicial/exercicios/ex013/script.js
@@ -2,6 +2,13 @@
 function calcula() {
     let num1 = parseFloat(prompt("Digite o primeiro número"))
     let num2 = parseFloat(prompt("Digite o primeiro número"))
-    
+
+    let res = document.querySelector("section#resultado")
+
+    if (num1 > num2) {
+        res.innerHTML = `<P>Analizando os valores <strong>${num1.toFixed(2)}</strong> e<strong> ${num2.toFixed(2)}</strong>, o maior valor é <mark>${num1.toFixed(2)}</mark>.</P>`
+    } else {
+        res.innerHTML = `<P>Analizando os valores<strong> ${num1.toFixed(2)}</strong> e <strong>${num2.toFixed(2)}</strong>, o maior valor é <mark>${num2.toFixed(2)}</mark></P>`
+    }
 
 }

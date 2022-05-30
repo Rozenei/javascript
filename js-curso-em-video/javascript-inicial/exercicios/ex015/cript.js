c82669a16009ef1aa800ab78dbf58185f54bb649
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-30 12:47:39 -0300

    js

--- js-curso-em-video/javascript-inicial/exercicios/ex015/cript.js
+++ js-curso-em-video/javascript-inicial/exercicios/ex015/cript.js
@@ -4,13 +4,23 @@ function dataHora() {
     let dias = new Array (" seg", "ter ", "qua ", "qui ", "sex ", "sab ", " dom",)
     let agora = mew Date
     let saida = document.querySelector("saida")
-    let dia = 
-    let mes = 
-    let ano = 
-    let sem = 
-    let hora = 
-    let min = 
-    let seg = 
-    let  = 
+    let dia = agora.getDate()
+    let mes = agora.getMonth()
+    let ano = agora.getFullYear()
+    let sem = agora.getDay()
+    let hora = agora.getHours()
+    let min = agora.getMinutes()
+    let seg = agora.getSeconds()
+
+    saida.innerHTML = `<P>Dia: <mark></mark>${}</P>` 
+    saida.innerHTML = `<P>Mês: <mark></mark>${}</P>` 
+    saida.innerHTML = `<P>Ano: <mark></mark>${}</P>` 
+    saida.innerHTML = `<P>Dia da semana: <mark></mark>${}</P>` 
+    saida.innerHTML = `<P>Hora: <mark></mark>${}</P>` 
+    saida.innerHTML = `<P>Minuto: <mark></mark>${}</P>` 
+    saida.innerHTML = `<P>Segundos: <mark></mark>${}</P>` 
+
+
+    
     
 }

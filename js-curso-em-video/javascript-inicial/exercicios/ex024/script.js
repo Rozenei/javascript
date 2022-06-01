85a617a3ca3b00a8c94a686663c69490f0e2e1f2
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-05-31 23:17:52 -0300

    js

--- js-curso-em-video/javascript-inicial/exercicios/ex024/script.js
+++ js-curso-em-video/javascript-inicial/exercicios/ex024/script.js
@@ -1,10 +1,10 @@
 
 function contar() {
     let saida = document.querySelector("section#saida")
-    saida.innerHTML += `<h2>Contando de 10 até 1</h2>`
 
+    saida.innerHTML += `<h2>Contagem regressiva de 10 até 1</h2>`
     let cont = 10
-    while (cont <= 10) {
+    while (cont >= 1) {
         saida.innerHTML += `${cont} &#x1F449;`
         cont--
     }

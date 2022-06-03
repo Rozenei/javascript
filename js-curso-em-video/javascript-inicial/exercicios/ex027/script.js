8b6e6e4b3e32bc57b21574cfe2cee32f7cd48b38
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-02 22:46:34 -0300

    js

--- js-curso-em-video/javascript-inicial/exercicios/ex027/script.js
+++ js-curso-em-video/javascript-inicial/exercicios/ex027/script.js
@@ -1,4 +1,17 @@
 
-function taboada() {
-    
+function tabuada() {
+    let saida = document.getElementById("saida")
+
+    let num = Number(document.getElementById("fnum").value)
+
+    saida.innerHTML += `<h2>Taboada do ${num}</h2>`
+    let cont = 1
+
+    while (cont <= 10) {
+        saida.innerHTML += `${num} x ${cont} = <strong>${num * cont}</strong><br>`
+        cont ++
+    }   
+
 }
+
+ 

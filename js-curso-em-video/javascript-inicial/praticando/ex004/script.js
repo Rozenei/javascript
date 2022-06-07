08d73e4136b739a6974a274d1bc4b0a1407632d7
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-07 17:27:52 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/ex004/script.js
+++ js-curso-em-video/javascript-inicial/praticando/ex004/script.js
@@ -1,14 +1,15 @@
 
 function verificar() {
     var data = new Date()
-    var ano = data.getFullYer()
+    var ano = data.getFullYear()
     var fano = document.getElementById("txtano")
     var res = document.getElementById("res")
     if (fano.value.length == 0 || fano.value > ano){
-        window.alert("Verifique os dados e tente novamente");
+        alert("Verifique os dados e tente novamente")
     }else {
-       window.alert("Tudo certo, pode começar");
+       var fsex = document.getElementsByName("radsex")
+       var idade = ano - Number(fano.value)
+       res.innerHTML = `A idade é ${idade} anos`
     }
-    
 
 }

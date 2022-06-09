447cc77ecb71f2ad1d2afcd3080bef46ee773898
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-09 11:38:18 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/ex004/script.js
+++ js-curso-em-video/javascript-inicial/praticando/ex004/script.js
@@ -9,7 +9,44 @@ function verificar() {
     }else {
        var fsex = document.getElementsByName("radsex")
        var idade = ano - Number(fano.value)
-       res.innerHTML = `A idade é ${idade} anos`
+       var genero = ""
+       var img = document.createElement("img")
+        img.setAttribute('id', 'foto')
+       if (fsex[0].checked){
+           genero = "Homem"
+           if (idade >= 0 && idade < 10){
+               //criança
+               img.setAttribute("src","imagem/menino_criança.jpg")
+           }else if (idade > 18 && idade <= 30) {
+               img.setAttribute("src", "imagem/homem18a30.jpg")
+               //jovem
+           }else if (idade > 31 && idade < 50) {
+               img.setAttribute("src" , "imagem/homem0a50.jpg")
+               //adulto
+           }else {
+               img.setAttribute("src", "imagem/homem60+.jpg")
+               //idoso
+           }
+       }else if (fsex[1].checked) {
+           genero = "Mulher"
+           if (idade >= 0 && idade < 10){
+               img.setAttribute("src","imagem/menina_criança.jpg")
+            //criança
+        }else if (idade > 18 && idade <= 30) {
+            img.setAttribute("src", "imagem/mulher18a30.jpg")
+            //jovem
+        }else if (idade > 30 && idade <= 50) {
+            img.setAttribute("src", "imagem/mulher30a50.jpg")
+            //adulto
+        }else {
+            img.setAttribute("src","imagem/mulher60+.jpg")
+            //idoso
+        }
+       }
+
+       res.innerHTML = `<P> Detectado ${genero} com ${idade} anos </P>`
+       
+       res.appendChild(img)
     }
 
 }

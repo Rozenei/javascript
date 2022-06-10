5a2d22f7b7de223ed870647aab607fa940cf0988
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-10 17:23:40 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/ex06/contar.js
+++ js-curso-em-video/javascript-inicial/praticando/ex06/contar.js
@@ -3,20 +3,14 @@ function contar() {
     let numIni = Number(document.getElementById("txti").value)
     let numFin = Number(document.getElementById("txtf").value)
     let pas = Number(document.getElementById("txtp").value)
-    let saida = document.getElementById("res")
+    
 
-    saida.innerHTML += `<P>Contando de ${numIni} até ${numFin} de ${pas} em ${pas}</P>`
-    
-    while (numIni <= numFin || numIni >= numFin) {
-        
-        if (numIni <= numFin) {
-            saida.innerHTML += `${numIni} &#x1F449; `
-            numIni += pas
-        } else if (numIni >= numFin) {
-            saida.innerHTML += `${numIni} &#x1F449; `
-            numIni -= pas
-        }
-        
+    if (numIni.value.length == 0 || numFin.value.length == 0 || pas.value.length == 0) {
+        alert("Erro")
+    }else {
+        alert("OK")
     }
-    
+     
 }
+    
+    

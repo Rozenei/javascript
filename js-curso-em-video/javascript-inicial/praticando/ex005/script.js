3630a5660c9fbffdbdae5ac6f219ec0b99a2447a
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-10 10:39:09 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/ex005/script.js
+++ js-curso-em-video/javascript-inicial/praticando/ex005/script.js
@@ -4,7 +4,7 @@ function tabuada() {
     let saida = document.getElementById("saida")
     let cont = 1 
     
-   saida.innerHTML = `<h2>Taboada do ${num}</h2>`
+   saida.innerHTML = `<h2>Tabuada do ${num}</h2>`
   while (cont <= 10) {
       saida.innerHTML += `<P>${num} x ${cont} = <strong>${num * cont}</strong></P>`
        cont++

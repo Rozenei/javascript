e89dffd8e1d4df72013bc80156055374441c28f6
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-13 17:25:23 -0300

    js

---
+++ js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/voto/voto.js
@@ -0,0 +1,13 @@
+
+function votar() {
+    let idade = document.getElementById("idad")
+    let saida = document.getElementById("saida")
+
+    if (idade > 15 && idade < 18 || idade > 70) {
+        saida.innerHTML = `O voto é opcional`
+    }else if (idade < 16) {
+        saida.innerHTML = `Não vota`
+    } else {
+        saida.innerHTML = `O voto é obrigatório`
+    }
+}
--- js-curso-em-video/javascript-inicial/praticando/ex06/contar.js
+++ js-curso-em-video/javascript-inicial/praticando/ex06/contar.js
@@ -1,16 +1,35 @@
 
 function contar() {
-    let numIni = Number(document.getElementById("txti").value)
-    let numFin = Number(document.getElementById("txtf").value)
-    let pas = Number(document.getElementById("txtp").value)
-    
+    let ini = document.getElementById("txti")
+    let fin = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById("res")
 
-    if (numIni.value.length == 0 || numFin.value.length == 0 || pas.value.length == 0) {
-        alert("Erro")
+    if (ini.value.length == 0 || fin.value.length == 0 || passo.value.length == 0) {
+        alert("[Erro] Verifique os dados e tente novamente!")
     }else {
-        alert("OK")
+        res.innerHTML = `<P>Contando</P>`
+        let numini = Number(ini.value)
+        let numfin = Number(fin.value)
+        let pas = Number(passo.value)
+        
+       if (pas <= 0) {
+            alert("Passo inválido! Considerar passo 1")
+            pas = 1
+       }
+
+        if (numini < numfin) {
+            //contagem crescente
+            for (let cont = numini; cont <= numfin; cont += pas) {
+            res.innerHTML += ` ${cont}, \u{1F449} `
+           }
+        } else {
+            //contagem regressiva
+            for (let cont = numini; cont >= numfin; cont -= pas) {
+                res.innerHTML += `${cont}, \u{1F449}`
+            }
+        }
+        res.innerHTML += ` \u{1F3c1} `   
     }
-     
+   
 }
-    
-    

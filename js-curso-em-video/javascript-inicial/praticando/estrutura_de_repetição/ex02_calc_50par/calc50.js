c4751896561495eff6237e67f3beb1c2fc4863ce
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-14 17:22:39 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex02_calc_50par/calc50.js
+++ js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex02_calc_50par/calc50.js
@@ -1,12 +1,16 @@
 
-function somar() {
-    let inicio = 2
-    let num  
-    let cont 
-
+function somar(x) {
     let res = document.getElementById("res")
-    for (num % 2 == 0) {
-        inicio += 2
-    }
-    res.innerHTML = `a soma é ${inicio}`
+    res.innerHTML += `<h2>Somando</h2> <br>`
+
+    let soma = 0
+    for(let cont = 2; cont <= x; cont += 2) {
+        
+        soma = soma + cont
+        res.innerHTML = `A soma é <br> ${soma(10)} `
+       
+       
+    }  
+   // res.innerHTML = `A soma é <br> ${soma} `
+   
 }

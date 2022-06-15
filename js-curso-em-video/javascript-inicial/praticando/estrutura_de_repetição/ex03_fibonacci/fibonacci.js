d46f2f29ca01b488abb965516833598c95655d79
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-15 16:47:32 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex03_fibonacci/fibonacci.js
+++ js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex03_fibonacci/fibonacci.js
@@ -4,22 +4,17 @@ function fibonacci(){
     res.innerHTML = `Sequência de Fibonacci`
     let x = 0
     let y = 1
-
-    let f = x 
+    let f ;
     
-   // let x = a + b // x = 0 + 1 / x = 1
-   // let y = x + x  // y = 1 + 1  / Y = 2
-   // let f = x + y // F = 1 + 2
-   
-    let cont = 0
+   cont = 3 
     while(cont < 10) {
-        
-        y = y + (y+x)
-        
-        res.innerHTML += ` <br> ${f} `
-        f = x+y
-        cont++
-        
+
+        f = y + x
+        x = y
+        y = f
+        cont++ 
+        res.innerHTML += ` ${f} ,  `
+       
     }
      
 }

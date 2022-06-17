c824121b8e2736daef0aadc7542f3141f03c09a2
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-17 15:52:51 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/ex07_varios_clac/varios_calc.js
+++ js-curso-em-video/javascript-inicial/praticando/ex07_varios_clac/varios_calc.js
@@ -8,37 +8,34 @@ function calcular() {
     let subt = n1 - n2
     let mult = n1 * n2
     let divi = n1 / n2
-    
-  
-   /* res.innerHTML = `Calculando...`
-    if(n1.value.length == 0 || n2.value.length ==0) {
-        alert("[ERRO! Verifique os dados e tente novamente")
-    }else {
+
+    res.innerHTML = `Calculando...`
+    if(n1.value.length == 0 || n2.value.length == 0) {
+        alert("ERRO")
+    }
+        let fcalc = document.getElementsByName("radcalc")
         n1 = Number(n1.value)
         n2 = Number(n2.value)
-        let fcalc = document.getElementsByName("radcalc")
-    }*/
-    n1 = Number(n1.value)
-    n2 = Number(n2.value)
-    let fcalc = document.getElementsByName("radcalc")
-
-    if(fcalc[0].checked){
+  
+     if(fcalc[0].checked){
         fcalc = soma
         soma = n1 + n2
         res.innerHTML += ` <P>O resultado de ${n1} + ${n2} é <strong><mark>${soma.toFixed(2)}</mark></P></strong>`
-    } if(fcalc[1].checked) {
+
+    }else if(fcalc[1].checked) {
         fcalc = subt
         subt = n1 - n2
         res.innerHTML += ` <P>O resultado de ${n1} - ${n2} é <strong><mark> ${subt.toFixed(2)}</P></mark></strong>`
+
     }if(fcalc[2].checked) {
         fcalc = mult
         mult = n1 * n2
         res.innerHTML += ` <P>O resultado de ${n1} * ${n2} é <strong><mark>${mult.toFixed(2)}</P></mark></strong>`
+
     }if(fcalc[3].checked) {
         fcalc = divi
         divi = n1 / n2
-
         res.innerHTML += `<P> O resultado de ${n1} / ${n2} é <strong><mark>${divi.toFixed(2)}</mark></P></strong`
     }
-      
+    
 }

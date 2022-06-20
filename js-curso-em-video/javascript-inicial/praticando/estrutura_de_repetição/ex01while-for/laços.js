540a5b7892a0af4d0a6f77e4f1b18c18987b1ba0
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-20 17:15:21 -0300

    js

---
+++ js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex01while-for/laços.js
@@ -0,0 +1,28 @@
+
+let img = document.createElement("img")
+img.setAttribute("id", "imagem")
+
+
+function lacosWhile() {
+    let While = document.getElementById("while")
+    let c = 1
+    while (c <= 10) {//enquanto c <= 10 faça isso
+        While.innerHTML += `<p>Testando laços com <strong>While</strong> </p>`
+        c++        
+    }   
+}
+
+function lacosDoWhile() {
+    let doWhile = document.getElementById("dowhile")
+    let c = 1
+    do {//faça isso
+        doWhile.innerHTML += `<p> Testando laços com <strong>do while</strong>`
+        c++
+    } while(c <= 10)//enquanto c <= 10
+      
+}
+function lacosFor() {
+    let For = document.getElementById("for")
+    for (let c = 1; c <= 10; c++)
+    For.innerHTML += `<p>Testando laços com <strong>For</strong></p>`
+}
--- js-curso-em-video/javascript-inicial/praticando/ex08_capital/capital.js
+++ js-curso-em-video/javascript-inicial/praticando/ex08_capital/capital.js
@@ -4,9 +4,16 @@ function capital() {
     res.innerHTML = `Verificando...`
 
     let est = document.getElementsByName("estado")
-    let pr = "Curitiba"
-    let sc = "Florianopolis"
-    let rs = "Porto Alegre"
-   
-    alert(pr)
+    let pr = "Paraná"
+    let sc = "Santa Catarina"
+    let rs = "Rio Grande do Sul"
+    
+    if (est = pr) {
+        res.innerHTML = `A capital se ${pr} é Curitiba `
+    } if (est = sc) {
+        res.innerHTML = `A capital se ${sc} é Florianópolis`
+    }if (est = rs) {
+        res.innerHTML = `A capital se ${rs} é Porto Alegre`
+    }
+
 }

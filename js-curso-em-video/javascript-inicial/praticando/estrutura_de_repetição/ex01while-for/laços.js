ab3e940c968fd26ced7873cd1b1b24d51a230aa5
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-21 17:28:54 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex01while-for/laços.js
+++ js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex01while-for/laços.js
@@ -1,37 +1,64 @@
 
-let img = document.createElement("img")
-img.setAttribute("id", "foto")
+
 
 
 function lacosWhile() {
-    let img = document.createElement("img")
-    img.setAttribute("id", "foto")
-    img.setAttribute("src", "imagem/while.PNG")
-
     let While = document.getElementById("while")
+    let x1 = Number(document.getElementById("num1").value) 
+    let texto = document.getElementById("while")
     let c = 1
    
-    while (c <= 10) {//enquanto c <= 10 faça isso
-    img.setAttribute("src", "imagem/while.PNG")
-        While.innerHTML += `<p>Testando laços com <strong>While</strong>  </p>`
+    while (c <= x1) {//enquanto c <= 10 faça isso
+        While.innerHTML += `<p> ${texto}  <strong>While</strong>  </p>`
         c++        
     }   
 }
 
 function lacosDoWhile() {
     let doWhile = document.getElementById("dowhile")
+    let x2 = Number(document.getElementById("num2").value) 
     let c = 1
     do {//faça isso
         doWhile.innerHTML += `<p> Testando laços com <strong>do while</strong> `
         c++
-    } while(c <= 10)//enquanto c <= 10
+    } while(c <= x2)//enquanto c <= 10
       
 }
 function lacosFor() {
     let For = document.getElementById("for")
-    for (let c = 1; c <= 10; c++)
+    let x3 = Number(document.getElementById("num3").value) 
+
+    for (let c = 1; c <= x3; c++)
+  
     For.innerHTML += `<p>Testando laços com <strong>For</strong></p>`
 }
-function imagens() {
+
+function imgWhile() {
+    let res = document.getElementById("res")
+    let img = document.createElement("img")
     
+    // img.setAttribute("id", "foto")
+   
+    img.setAttribute("src",  "imagem/while.PNG")
+    res.appendChild(img)
+   
+
+}
+function imgDoWhile() {
+    let res = document.getElementById("res2")
+    let img = document.createElement("img")
+    
+    // img.setAttribute("id", "foto")
+
+    img.setAttribute("src", "imagem/do_while.PNG") 
+    res.appendChild(img)
+}
+function imgFor() {
+    let res = document.getElementById("res3")
+    let img = document.createElement("img")
+
+    // img.setAttribute("id", "foto")
+
+    img.setAttribute("src","imagem/for.PNG")    
+    res.appendChild(img)
 }

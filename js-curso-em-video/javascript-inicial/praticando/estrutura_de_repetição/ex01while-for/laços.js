336cc4cb4834d81f3e59aa7e71c67a11f9fc0a5f
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-06-22 16:01:01 -0300

    js

--- js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex01while-for/laços.js
+++ js-curso-em-video/javascript-inicial/praticando/estrutura_de_repetição/ex01while-for/laços.js
@@ -1,15 +1,12 @@
 
-
-
-
 function lacosWhile() {
     let While = document.getElementById("while")
     let x1 = Number(document.getElementById("num1").value) 
-    let texto = document.getElementById("while")
+    let texto = document.getElementById("txt1").value
     let c = 1
    
     while (c <= x1) {//enquanto c <= 10 faça isso
-        While.innerHTML += `<p> ${texto}  <strong>While</strong>  </p>`
+        While.innerHTML += `<p> <strong>${texto}</strong>  </p>`
         c++        
     }   
 }
@@ -17,48 +14,53 @@ function lacosWhile() {
 function lacosDoWhile() {
     let doWhile = document.getElementById("dowhile")
     let x2 = Number(document.getElementById("num2").value) 
+    let texto2 = document.getElementById("txt2").value
     let c = 1
     do {//faça isso
-        doWhile.innerHTML += `<p> Testando laços com <strong>do while</strong> `
+        doWhile.innerHTML += `<p> <strong>${texto2}</strong> </p> `
         c++
-    } while(c <= x2)//enquanto c <= 10
-      
+    } while(c <= x2)//enquanto c <= 10     
 }
 function lacosFor() {
     let For = document.getElementById("for")
     let x3 = Number(document.getElementById("num3").value) 
-
-    for (let c = 1; c <= x3; c++)
-  
-    For.innerHTML += `<p>Testando laços com <strong>For</strong></p>`
+    let texto3 = document.getElementById("txt3").value
+    
+    for(let c = 1; c<= x3; c++)
+    For.innerHTML += `<p> <strong>${texto3}</strong> </p>`
 }
-
 function imgWhile() {
     let res = document.getElementById("res")
     let img = document.createElement("img")
     
-    // img.setAttribute("id", "foto")
-   
-    img.setAttribute("src",  "imagem/while.PNG")
+    img.setAttribute("src",  "imagem/codigo_while.PNG")
     res.appendChild(img)
-   
-
 }
 function imgDoWhile() {
     let res = document.getElementById("res2")
     let img = document.createElement("img")
     
-    // img.setAttribute("id", "foto")
-
-    img.setAttribute("src", "imagem/do_while.PNG") 
+    img.setAttribute("src", "imagem/codigo_dowhile.PNG") 
     res.appendChild(img)
 }
 function imgFor() {
     let res = document.getElementById("res3")
     let img = document.createElement("img")
 
-    // img.setAttribute("id", "foto")
+    img.setAttribute("src","imagem/codigo_for.PNG")    
+    res.appendChild(img)
+}
+function codhtml() {
+    let res = document.getElementById("reshtml")
+    let img = document.createElement("img")
 
-    img.setAttribute("src","imagem/for.PNG")    
+    img.setAttribute("src", "imagem/cod_html.PNG")
+    res.appendChild(img)
+}
+function codcss() {
+    let res = document.getElementById("rescss") 
+    let img = document.createElement("img")
+
+    img.setAttribute("src", "imagem/cod_css.PNG")
     res.appendChild(img)
 }
